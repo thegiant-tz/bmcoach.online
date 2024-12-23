afd17e300ec978021c8f3def5b16baa2b7baad1a
Shebyclassic2018 <shaabanbakhresa85@gmail.com>
2024-12-23 10:59:54 +0300

    boarding points

--- app/Http/Controllers/API/TimetableController.php
+++ app/Http/Controllers/API/TimetableController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BookingResource;
-use App\Models\Bus;
 use App\Models\Timetable;
 use Illuminate\Http\Request;
 
